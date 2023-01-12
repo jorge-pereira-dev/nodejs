900d879a8cf8237cb9fdbe1f4a8e831ccae9704c
Jorge Luis Ferreira Nunes Pereira <jorge.pereira@araguaia.com.br>
2023-01-12 18:51:54 -0300

    teste 2

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express'); 
+require('dotenv').config();
 const app = express();              
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname, { index: false }));
+
+app.listen(process.env.PORT || 3000, function() {       
+    var port = (process.env.PORT) ? process.env.PORT : "3000";
+    console.log("Server running on port " + port);
+});
+
 
 app.get('/', (req, res) => {        
     res.sendFile(__dirname + "/index.html");  
 });
 
-app.listen(process.env.PORT || 3000, function() {       
-    var port = (process.env.PORT) ? process.env.PORT : "3000";
-    console.log("Server running on port " + port);
+var data = { 
+    date: Date().toLocaleString(), 
+    ambte: process.env.AMBTE 
+};
+
+app.post("/redirect",  function(req, res){
+    res.render("redirect", {
+        data: data
+    });
+});
+
+app.post("/return",  function(req, res){
+    res.redirect("/");
 });
